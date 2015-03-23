35a44c34660d9e8f27d6ee2e58917842984ec059
Bertrand Marron <bertrand.marron@ionisx.com>
2015-03-23 20:30:27 +0100

    Update database

--- scripts/init.sh
+++ scripts/init.sh
@@ -28,6 +28,22 @@ log "Update Open edX LMS assets"
 
 echo
 
+log "Set staff as superuser"
+echo "update auth_user set is_superuser=1 where username='staff'" \
+    | mysql -u root edxapp \
+    && log Done \
+    || fail Failed
+
+echo
+
+log "Update darklang config"
+echo "insert into dark_lang_darklangconfig set change_date=curtime(), changed_by_id=1, enabled=1, released_languages='en,fr';"
+    | mysql -u root edxapp \
+    && log Done \
+    || fail Failed
+
+echo
+
 log "Run asset watcher"
 pkill -9 grunt
 nohup grunt > theme.log 2> theme.err < /dev/null &
